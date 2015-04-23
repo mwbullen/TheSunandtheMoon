973c8194fcfa40caa561cb7d7a851a3b613148d9
mwbullen <mwbullen@gmail.com>
2015-04-23 14:42:42 -0400

    Speed throttling for moon

---
+++ Assets/ObjectThrottle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectThrottle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public float getSquareRoot(float f) {
+		return Mathf.Sqrt (f);
+
+	}
+}
