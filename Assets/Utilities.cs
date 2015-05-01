89a7f4a24b39bd4da9bb634af521401fded010fc
mwbullen <mwbullen@gmail.com>
2015-05-01 14:38:55 -0400

    Ping style paddle physics

---
+++ Assets/Utilities.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Utilities : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public Vector2 makeVector2(float x, float y) {
+		return new Vector2 (x, y);
+
+	}
+
+	public Vector2 multiplyVector2(Vector2 inputVector, float speed) {
+		return inputVector * speed;
+	}
+}
