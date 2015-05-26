08d60888fbb06cb36032b30664b65abad180ec06
mwbullen <mwbullen@gmail.com>
2015-05-26 11:41:35 -0400

    Adding ads

---
+++ Assets/Scripts/AdsWrapper.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Advertisements;
+
+public class AdsWrapper : MonoBehaviour {
+	public ShowOptions adShowOptions;
+	// Use this for initializat	ion
+	void Start () {
+		Advertisement.Initialize ("37512");
+
+		adShowOptions = new ShowOptions ();
+		adShowOptions.pause = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void PlayAd() {
+		if(Advertisement.isReady()){
+			Advertisement.Show(null, adShowOptions);
+		}
+	}
+}
